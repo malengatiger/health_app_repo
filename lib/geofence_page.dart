f93425675ec47224c26ead42eb6616559611bb44
malengatiger <malengatiger@gmail.com>
2021-05-13 23:58:03 +0200

    initial commit

---
+++ lib/geofence_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_geofence/Geolocation.dart';
+import 'package:flutter_geofence/geofence.dart';
+import 'package:health_app_repo/geofencer.dart';
+
+class GeofencePage extends StatefulWidget {
+  @override
+  _GeofencePageState createState() => _GeofencePageState();
+}
+
+class _GeofencePageState extends State<GeofencePage>
+    with SingleTickerProviderStateMixin
+    implements GeofencerListener {
+  AnimationController _controller;
+  static const mm = '🌸 🌸 🌸 🌸 🌸  GeofencePage: ';
+  Geofencer _geofencer = geofencer;
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this);
+    super.initState();
+    _getCurrentPosition();
+    _startFences();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _getCurrentPosition() async {
+    debugPrint('$mm ..... _getCurrentPosition starting ............');
+    var pos = await _geofencer.getCurrentPosition();
+    debugPrint('$mm _getCurrentPosition: 🍎 result from _getCurrentPosition:  '
+        '🅿️ latitude: ${pos.latitude}  🅿️ longitude: ${pos.longitude}  🅿️ ');
+  }
+
+  void _startFences() async {
+    debugPrint('$mm ..... startFences will happen soon ....');
+    var result = await _geofencer.buildGeofences(this);
+    debugPrint(
+        '$mm ..... startFences: 🍎 result from buildGeofences: $result 🍎');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Geofencer',
+          style: TextStyle(
+              fontWeight: FontWeight.w100, color: Colors.white, fontSize: 14),
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                _getCurrentPosition();
+                _startFences();
+              })
+        ],
+      ),
+    );
+  }
+
+  @override
+  onGeofenceEntry(Geolocation geolocation) {
+    debugPrint(
+        '$mm onGeofenceEntry: geoLocation at ENTRY: 🔵  lat: ${geolocation.latitude} 🔵 lng: ${geolocation.longitude} 💚 💚 💚');
+  }
+
+  @override
+  onGeofenceExit(Geolocation geolocation) {
+    debugPrint(
+        '$mm onGeofenceExit: geoLocation at EXIT:  🔵 lat: ${geolocation.latitude} 🔵 lng: ${geolocation.longitude} ❤️ ❤️ ❤️');
+  }
+
+  @override
+  onBackgroundLocation(Coordinate coordinate) {
+    debugPrint(
+        '$mm onBackgroundLocation: coordinates: 🥦 lat: ${coordinate.latitude} 🥦 lng: ${coordinate.longitude} 🥦 ');
+  }
+}
---
+++ lib/geofencer.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_geofence/geofence.dart';
+import 'package:geolocator/geolocator.dart';
+
+final Geofencer geofencer = Geofencer.instance;
+
+abstract class GeofencerListener {
+  onGeofenceEntry(Geolocation geolocation);
+  onGeofenceExit(Geolocation geolocation);
+  onBackgroundLocation(Coordinate coordinate);
+}
+
+class Geofencer {
+  static const mm = '💦💦💦💦  Geofencer: ';
+
+  Geofencer._privateConstructor() {
+    Geofence.initialize();
+    debugPrint(
+        '$mm Geofencer has been initialized : 🌺 ${DateTime.now().toIso8601String()} 🌺');
+  }
+
+  static final Geofencer instance = Geofencer._privateConstructor();
+
+  Future buildGeofences(GeofencerListener listener) async {
+    debugPrint('$mm will be building geofences: requesting permission ...');
+    Geofence.requestPermissions();
+    var pos = await getCurrentPosition();
+    var location = Geolocation(
+        latitude: pos.latitude,
+        longitude: pos.longitude,
+        radius: 200,
+        id: 'myLocation');
+    await Geofence.addGeolocation(location, GeolocationEvent.entry)
+        .then((onValue) {
+      //scheduleNotification("Georegion added", "Your geofence has been added!");
+      debugPrint('$mm Geofence has been added for ENTRY: 💙 💙 💙 ');
+    }).catchError((error) {
+      print("failed with $error");
+      throw Exception('😈 👿 We are fucked, Boss! $error');
+    });
+    await Geofence.addGeolocation(location, GeolocationEvent.exit)
+        .then((onValue) {
+      //scheduleNotification("Georegion added", "Your geofence has been added!");
+      debugPrint('$mm Geofence has been added for EXIT:  ❤️ ❤️ ❤️');
+    }).catchError((error) {
+      print("failed with $error");
+      throw Exception('😈 👿 We are fucked, Boss! $error');
+    });
+
+    Geofence.startListening(GeolocationEvent.entry, (entry) {
+      //scheduleNotification("Entry of a georegion", "Welcome to: ${entry.id}");
+      debugPrint('$mm Geofence has been fired for ENTRY:  🛎 🛎 🛎 🛎 $entry');
+      listener.onGeofenceEntry(entry);
+    });
+
+    Geofence.startListening(GeolocationEvent.exit, (exit) {
+      //scheduleNotification("Entry of a georegion", "Welcome to: ${entry.id}");
+      debugPrint('$mm Geofence has been fired for EXIT:  🛎 🛎 🛎 🛎 $exit');
+      listener.onGeofenceEntry(exit);
+    });
+    Geofence.backgroundLocationUpdated.stream.listen((event) {
+      // scheduleNotification("You moved significantly", "a significant location change just happened.");
+      debugPrint(
+          '$mm Geofence backgroundLocationUpdated: 🔷 🔷 🔷 🔷  ${event.latitude} ${event.longitude}');
+      listener.onBackgroundLocation(event);
+    });
+
+    return '✅ ✅ Geofence has been added for both enter and exit ✅ ✅ listeners set up 🔺🔺🔺';
+  }
+
+  Future<Position> getCurrentPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('😈 👿 Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('😈 👿 Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          '😈 👿 Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    var pos = await Geolocator.getCurrentPosition();
+    debugPrint(
+        '$mm Position via Geolocator: 🌀 🌀 latitude: ${pos.latitude} 🌀 🌀 longitude: ${pos.longitude}');
+
+    return pos;
+  }
+}
